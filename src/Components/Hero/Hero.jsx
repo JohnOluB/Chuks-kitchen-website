c64faad91e5049e12b446a9401971dcbf9b8d74b
JohnOluB <adebayodeolu90@gmail.com>
2026-02-20 22:33:28 +0100

    feat: Style header section with Island Moments font and spacing

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -4,12 +4,12 @@ const Hero = () => {
   return (    
     <div className='flex'>
       <img src={BgImage} alt="background-image" className='w-1/2 h-[1024px]'/>
-        <div className='ml-[300px]'>
-            <span>chuks kitchen</span>                
-            <button className='w-36 h-12 border-2 border-[#1E88E5] rounded-lg text-base text-[#1E88E5]'>Sign In</button>
+        <header className='ml-[100px] mt-[20px]'>
+            <span className='font-[Island_Moments] text-[#FF7A18] text-4xl'>Chuks Kitchen</span>                
+            <button className='w-36 h-12 border-2 border-[#1E88E5] rounded-lg text-base text-[#1E88E5] ml-[200px]'>Sign In</button>
         
-        
-        </div>
+  
+        </header>
     </div>
   )
 }
