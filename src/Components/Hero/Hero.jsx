d024bd513ae6f7026df11d82d2fbc70c57001bbb
JohnOluB <adebayodeolu90@gmail.com>
2026-02-20 22:36:25 +0100

    feat: add font weight to sign in button

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -6,7 +6,7 @@ const Hero = () => {
       <img src={BgImage} alt="background-image" className='w-1/2 h-[1024px]'/>
         <header className='ml-[100px] mt-[20px]'>
             <span className='font-[Island_Moments] text-[#FF7A18] text-4xl'>Chuks Kitchen</span>                
-            <button className='w-36 h-12 border-2 border-[#1E88E5] rounded-lg text-base text-[#1E88E5] ml-[200px]'>Sign In</button>
+            <button className='w-36 h-12 border-2 border-[#1E88E5] rounded-lg text-base text-[#1E88E5] ml-[200px] font-medium'>Sign In</button>
         
   
         </header>
