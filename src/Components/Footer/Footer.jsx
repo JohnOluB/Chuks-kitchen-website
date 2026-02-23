f1353f1dabe86956e4f798858db88592839a794e
JohnOluB <adebayodeolu90@gmail.com>
2026-02-23 06:46:06 +0100

    feat: update font via link rel tag and adjust footer styling

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -3,34 +3,43 @@ import React from "react";
 export default function Footer() {
 return(
     <>
-    <div className='flex justify-space between bg-Brown-300 h-[350px]'>
-      <div>
-        <span className='font-[Island_Moments] text-[#FF7A18] '>Chuks Kitchen</span>
+    <div className='flex justify-evenly h-[350px] bg-[rgba(90,40,9,0.93)]/93 font-[poppins] '>
+      <div className="mt-5">
+        <span className='font-[Island_Moments] text-[#FF7A18] text-[37px]'>Chuks Kitchen</span>
+        <div className="text-white">
         <p>Bringing the authentic </p>
         <p>flavors of Nigerian</p> 
         <p>home cooking to your</p>
         <p>table, with passion</p> 
         <p>and care</p>
+        </div>
       </div>
-      <div>
-        <p>Quick Links</p>
+      <div className="text-white mt-5">
+        <p className="text-[24px]">Quick Links</p>
+        <div className="text-[12px]">
         <p>Home</p>
         <p>Explore</p>
         <p>My Order</p>
         <p>Account</p>
         <p>Contact</p>
+        </div>
     </div>
-      <div>
-        <p>Contact Us</p>
+      <div className='text-white mt-5'>
+        <p className="text-[24px]">Contact Us</p>
+        <div className="text-[12px] my-4">
+
         <p>+234 801 234 5678</p>
         <p>hello@chukskitchen.com</p>
         <p>123 Taste Blvd, Lagos, Nigeria</p>
+        </div>
     </div>
-    <div>
+    <div className="text-white mt-5">
+        <div className="text-[12px]">
         <p>Facebook</p>
         <p>Twitter</p>
         <p>Linkdein</p>
         <p>Instagram</p>
+        </div>
     </div>
     </div>
     </>
