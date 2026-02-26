9b83f207705b033a6ce73d97cfea6581266e16ca
JohnOluB <adebayodeolu90@gmail.com>
2026-02-26 03:12:16 +0100

    feat: add styling to hero components

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -17,20 +17,31 @@ export default function Hero(){
                             <span>Cooking</span>
                         </div>
                         <p className='font-[500] text-[28px] my-6'>Handcrafted with passion, delivered with care.</p>
-                        <button className='bg-[#FF7A18] h-[46px] w-[210px] rounded-lg mb-4'>Discover what's new</button>
+                        <button 
+                        className='bg-[#FF7A18] h-[46px] w-[210px] rounded-lg mb-4'>
+                        Discover what's new
+                        </button>
                     </div>
-                    
-                        <span className='bg-white'>
-                            <input type="text" placeholder='What are you craving for today?' className='text-black font-[500] text-[18px] w-[970px] p-15' /><img src={SearchIcon} alt="search icon" className='mt-[-4]'/>
-                        </span>
-                    
+                    <div className='relative w-[1190px] ml-[80px] mt-[250px]'>
+                        <input 
+                        type="text" 
+                        placeholder='What are you craving for today?' 
+                        className='bg-white font-medium text-black font-[500] text-[18px] w-[1190px] p-[10px] pl-14 rounded-lg placeholder:font-[600]' />
+                        <img src={SearchIcon} alt="search icon"                             className='absolute left-3 top-1/2 -translate-y-1/2 mt-[-4] w-[30px]'/>
+                    </div>
                 </div>
             </div>
-            
-            
-            
+            {/* <div className='flex flex-col justify-center items-center'>
+                <p className='font-[600] text-black h-full'>Popular Categories</p>
         
-            
+                <img src={} alt="" />
+                <img src={} alt="" />
+                <img src={} alt="" />
+                <img src={} alt="" />
+                <img src={} alt="" />
+                        
+                
+            </div>  */}
             
             <Footer/>
         </div>
