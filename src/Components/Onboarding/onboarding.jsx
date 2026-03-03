67fdbf0ce3df83502771a4881cfac8ecde549f2b
JohnOluB <adebayodeolu90@gmail.com>
2026-03-03 23:32:50 +0100

    fix: adjust background image width

--- src/Components/Onboarding/onboarding.jsx
+++ src/Components/Onboarding/onboarding.jsx
@@ -3,20 +3,19 @@ import BgImage from "../../assets/images/bg.png"
 import ForknKnife from '../../assets/Icons/fork-knife-fill.png'
 import Logistics from '../../assets/Icons/truck.png'
 import Footer from '../Footer/Footer'
-import { useNavigate } from 'react-router-dom'// import SignIn from '../SignIn/signIn'
+import { useNavigate } from 'react-router-dom'
 
 
 const OnBoarding = () => {
   const navigate = useNavigate()
   return (    
     <>
-    <div className='flex font-[Inter]'>
-      <img src={BgImage} alt="background-image" className='w-1/2 h-[1024px]'/>
+    <div className='flex font-[Inter] w-1/2'>
+      <img src={BgImage} alt="background-image" className='w-full object-cover h-[1024px]'/>
       <div className='flex-col ml-[100px]'>
         <header className=' mt-[20px]'>
-            <span className='font-[Island_Moments] text-[#FF7A18] text-4xl'>Chuks Kitchen</span>   
-            
-              <button onClick={() => navigate('/SignIn/signIn')} className='w-36 h-12 border-2 border-[#1E88E5] rounded-lg text-base text-[#1E88E5] ml-[200px] font-medium'>Sign In</button>  
+          <span className='font-[Island_Moments] text-[#FF7A18] text-4xl'>Chuks Kitchen</span>                
+          <button onClick={() => navigate('/SignIn/signIn')} className='w-36 h-12 border-2 border-[#1E88E5] rounded-lg text-base text-[#1E88E5] ml-[140px] font-medium'>Sign In</button>  
         </header>
         <div className='flex-col mt-[80px] font-[Inter]'>
           <p className='text-[32px] font-semibold'>Your Authentic Taste of Nigeria</p>
@@ -44,8 +43,8 @@ const OnBoarding = () => {
             <span className='flex justify-center items-center ml-[10px] '>Fast & Reliable Delivery</span>
           </div>
         </div>
-        <button className='h-12 w-[500px] border-2 bg-[#FF7A18] text-white rounded-lg mt-[38px]'>Start Your Order</button><br/>
-        <button className='h-12 w-[500px] border-2 border-[#1E88E5] text-[#1E88E5] rounded-lg mt-[24px]'>Learn More About Us</button>
+        <button onClick={() => navigate('/SignIn/signIn')} className='h-12 w-[500px] border-2 bg-[#FF7A18] text-white rounded-lg mt-[38px]'>Start Your Order</button><br/>
+        <button onClick={() => navigate('/SignIn/signIn')} className='h-12 w-[500px] border-2 border-[#1E88E5] text-[#1E88E5] rounded-lg mt-[24px]'>Learn More About Us</button>
           <p className='font-medium text-[15px] mt-[80px] flex justify-center items-center'>© 2024 Chuks Kitchen.  
           <span className='text-[#64B5F6] ml-[12px]'>Privacy Policy</span>
           <span className='text-[#64B5F6] ml-[12px]'>Terms of Service</span> 
