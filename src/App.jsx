18a13776a87d5061281353964bc20f031c3238b1
JohnOluB <adebayodeolu90@gmail.com>
2026-03-04 18:18:57 +0100

    feat: remove folder paths

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import OnBoarding from "./Components/Onboarding/onboarding"
 // import Footer from "./Components/Footer/Footer"
 import SignIn from "./Components/SignIn/signIn"
-// import SignUp from "./Components/Sign Up/signUp"
+import SignUp from "./Components/SignUp/signUp"
 import Hero from "./Components/Hero/Hero"
 // import Explore from "./Components/Explore/Explore"
 import { Routes, Route } from 'react-router-dom'
@@ -12,8 +12,9 @@ export default function App() {
   return (
     <Routes>
       <Route path="/" element={<OnBoarding />} />
-      <Route path="/SignIn/signIn" element={<SignIn />} />
-      <Route path="/Hero/hero" element={<Hero />} />
+      <Route path="/signin" element={<SignIn />} />
+      <Route path="/signup" element={<SignUp />} />
+      <Route path="/hero" element={<Hero />} />
     </Routes>
   )
 }
