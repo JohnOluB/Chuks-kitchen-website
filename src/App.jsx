2788da50c006c8f42989c8c5c166a7bbc128de9b
JohnOluB <adebayodeolu90@gmail.com>
2026-03-04 17:26:45 +0100

    feat: adjust component imports

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,10 @@ import OnBoarding from "./Components/Onboarding/onboarding"
 // import Footer from "./Components/Footer/Footer"
 import SignIn from "./Components/SignIn/signIn"
 // import SignUp from "./Components/Sign Up/signUp"
-// import Hero from "./Components/Hero/Hero"
+import Hero from "./Components/Hero/Hero"
 // import Explore from "./Components/Explore/Explore"
 import { Routes, Route } from 'react-router-dom'
-// import SignIn from "./Components/Sign In/signIn.jsx"
+
 
 
 export default function App() {
@@ -13,6 +13,7 @@ export default function App() {
     <Routes>
       <Route path="/" element={<OnBoarding />} />
       <Route path="/SignIn/signIn" element={<SignIn />} />
+      <Route path="/Hero/hero" element={<Hero />} />
     </Routes>
   )
 }
