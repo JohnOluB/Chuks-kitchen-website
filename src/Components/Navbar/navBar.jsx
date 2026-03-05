2bb81e74a779e130b79a41e236d22a9897bab5b5
JohnOluB <adebayodeolu90@gmail.com>
2026-03-05 22:43:39 +0100

    feat: add active routing to navbar using NavLink

--- src/Components/Navbar/navBar.jsx
+++ src/Components/Navbar/navBar.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useNavigate } from 'react-router-dom'
+import { NavLink, useNavigate } from 'react-router-dom'
 
 export default function NavBar(){
     const navigate = useNavigate()
@@ -7,10 +7,18 @@ export default function NavBar(){
         <div>
             <header className='flex justify-evenly mt-5'>
                 <span className='font-[Island_Moments] text-[#FF7A18] text-4xl'>Chuks Kitchen</span>                
-                <span>Home</span>
-                <span>Explore</span>
-                <span>My Orders</span>
-                <span>Account</span>
+                <NavLink to='/hero' className={({isActive})=> isActive ? 'text-[#FF7A18]' : 'text-black'}>
+                    Home
+                </NavLink>
+                <NavLink to='/explore' className={({isActive})=> isActive ? 'text-[#FF7A18]' : 'text-black'}>
+                    Explore
+                </NavLink>
+                <NavLink to='/my-order' className={({isActive})=> isActive ? 'text-[#FF7A18]' : 'text-black'}>
+                    My Orders
+                </NavLink>
+                <NavLink to='/' className={({isActive})=> isActive ? 'text-[#FF7A18]' : 'text-black'}>
+                    Account
+                </NavLink>
                 <button onClick={() => navigate('/signIn')} className='bg-[#FF7A18] h-[40px] w-[120px] rounded-lg text-[#ffffff] mb-4'>Login</button>
             </header>
         </div>
