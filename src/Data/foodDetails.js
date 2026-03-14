c2c3ec19a0dde849cb3bae9caedc2eaf2a11c48e
JohnOluB <adebayodeolu90@gmail.com>
2026-03-14 21:26:38 +0100

    feat: add food details component

---
+++ src/Data/foodDetails.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+
+// const foodDetails = [
+//     {
+//         id: 1
+//     }
+// ]
+
+function details(){
+    return(
+        <div>
+            <p>Win</p>
+        </div>
+    )
+}
+
+export default details
